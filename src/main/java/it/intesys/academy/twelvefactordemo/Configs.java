55ec6979fcbb83db86b0747aaa5bec0b0ac1d471
Enrico Costanzi <enrico.costanzi@intesys.it>
2023-05-11 15:25:05 +0200

    3. configs right

--- src/main/java/it/intesys/academy/twelvefactordemo/Configs.java
+++ src/main/java/it/intesys/academy/twelvefactordemo/Configs.java
@@ -1,39 +1,76 @@
 package it.intesys.academy.twelvefactordemo;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
+
 public class Configs {
 
-    /**
-     * Test environment
-     */
-    public static final String CUSTOM_MESSAGE_TEST = "Hello World Test!";
-    public static final int SERVER_PORT_TEST = 8080;
-    public static final String DATABASE_PASSWORD_TEST = "abc1234";
+    static Properties properties = new Properties();
+    static {
+        try {
 
-    /**
-     * Prod environment
-     */
-    public static final String CUSTOM_MESSAGE_PROD = "Hello World Prod!";
-    public static final int SERVER_PORT_PROD = 9090;
-    public static final String DATABASE_PASSWORD_PROD = "abc1234";
+            Properties classPathProperties = new Properties();
+            Properties localProperties = new Properties();
+            Properties fileSystemProperties = new Properties();
 
+            // read properties from the application.properties
+            InputStream resource = Configs.class.getClassLoader()
+                    .getResourceAsStream("application.properties");
+            classPathProperties.load(resource);
 
+            // read properties from the non versioned application-local.properties
+            InputStream localResource = Configs.class.getClassLoader()
+                    .getResourceAsStream("application-local.properties");
+            if (localResource != null) {
+                localProperties.load(localResource);
+            }
 
+            //read properties from file system
+            Path fileSystemPropertiesPath = Path.of("config", "application.properties");
+            if (Files.exists(fileSystemPropertiesPath)) {
+                InputStream config = Files.newInputStream(fileSystemPropertiesPath);
+                fileSystemProperties.load(config);
+            }
 
-    public static String getCustomMessage() {
-        String env = System.getenv("environment");
-        if (env != null && env.equals("prod")) {
-            return CUSTOM_MESSAGE_PROD;
-        } else {
-            return CUSTOM_MESSAGE_TEST;
+            // file system properties override classpath properties
+            properties.putAll(classPathProperties);
+            properties.putAll(localProperties);
+            properties.putAll(fileSystemProperties);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot read properties", e);
         }
     }
 
-    public static int getServerPort() {
-        String env = System.getenv("environment");
-        if (env != null && env.equals("prod")) {
-            return SERVER_PORT_PROD;
-        } else {
-            return SERVER_PORT_TEST;
+    public static String getStringProperty(String property) {
+
+        String prop = System.getenv(property);
+        if (prop != null) {
+            return prop;
         }
+
+        if (properties.getProperty(property) != null) {
+            return properties.getProperty(property);
+        }
+
+        return null;
     }
+
+    public static Integer getIntegerProperty(String property) {
+
+        String prop = System.getenv(property);
+
+        if (prop != null) {
+            return Integer.parseInt(prop);
+        }
+
+        if (properties.getProperty(property) != null) {
+            return Integer.parseInt(properties.getProperty(property));
+        }
+
+        return null;
+    }
+
 }
