af3b22121128cb3c0ee01a06514074e4b0d0241f
Enrico Costanzi <enrico.costanzi@intesys.it>
2023-05-11 15:08:28 +0200

    remove config comment

--- src/main/java/it/intesys/academy/twelvefactordemo/TwelveFactorApp.java
+++ src/main/java/it/intesys/academy/twelvefactordemo/TwelveFactorApp.java
@@ -6,7 +6,7 @@ import io.javalin.Javalin;
 public class TwelveFactorApp {
 
 	public static void main(String[] args) {
-		Javalin app = Javalin.create(/*config*/)
+		Javalin app = Javalin.create()
 				.get("/", ctx -> ctx.result("Hello World"))
 				.start(8080);
 	}
