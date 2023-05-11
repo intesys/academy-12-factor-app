fcccc95461a829ec2de401fce50d097cea81e420
Enrico Costanzi <enrico.costanzi@intesys.it>
2023-05-11 17:46:55 +0200

    4. backing services

---
+++ src/main/java/it/intesys/academy/twelvefactordemo/NewFileRequest.java
@@ -0,0 +1,23 @@
+package it.intesys.academy.twelvefactordemo;
+
+public class NewFileRequest {
+
+    private String fileName;
+    private String fileContent;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileContent() {
+        return fileContent;
+    }
+
+    public void setFileContent(String fileContent) {
+        this.fileContent = fileContent;
+    }
+}
